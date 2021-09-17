c1ab00567f4f84cd524f180f22b3ca9ee3964f46
remus432 <brosdev1@gmail.com>
2021-09-17 11:18:31 +0300

    accessibility changes 2

--- src/components/Activity.vue
+++ src/components/Activity.vue
@@ -6,7 +6,7 @@
     <div class="activity__card">
       <div class="activity__name">
         <span>{{ title }}</span>
-        <img src="/images/icon-ellipsis.svg" />
+        <img src="/images/icon-ellipsis.svg" alt="Ellipsis Icon" />
       </div>  
       <div class="activity__time">
         <h2 class="activity__time-current">{{ currTimeframe }}hrs</h2>
